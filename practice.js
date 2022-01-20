c2b874e51f1ca5c22925feefeca54039034390a8
Shokre <shokreserhan123@gmail.com>
2022-01-20 13:03:03 +0200

    fix deepclone

--- practice.js
+++ practice.js
@@ -51,35 +51,103 @@
 //Q3 
 //Implement Merge Sort
 
-let mergeSort = function(arr){
-    let mid = arr.length / 2
+// let mergeSort = function(arr){
+//     let mid = arr.length / 2
     
-    if(arr.length <= 1){
-        return arr
-    }else{
-        return merge(mergeSort(arr.splice(0,mid)), mergeSort(arr))
-    }
+//     if(arr.length <= 1){
+//         return arr
+//     }else{
+//         return merge(mergeSort(arr.splice(0,mid)), mergeSort(arr))
+//     }
+// }
+
+// let merge = function(leftArray, rightArray){
+//     let sortedArr = []
+    
+//     while(leftArray.length || rightArray.length){
+//         if(leftArray.length && rightArray.length){
+//             if(leftArray[0] < rightArray[0]){
+//                 sortedArr.push(leftArray.shift())
+//             }else{
+//                 sortedArr.push(rightArray.shift())
+//             }
+//         }else if(rightArray.length){
+//             sortedArr.push(rightArray.shift())
+//         }else if(leftArray.length){
+//             sortedArr.push(leftArray.shift())
+//         }
+//     }
+//     return sortedArr;
+// }
+
+// let arr = [11,1,2,10,5,4,6,7,8,2,9,3,4,1,1,1,1,1];
+
+// console.log(mergeSort(arr))
+
+
+const deepCloneObj = (obj) => {
+    let result = {}
+    let tempArray = []
+    Object.keys(obj).forEach(key => {
+        // console.log(typeof obj[key]);
+        if(typeof obj[key] !== "object"){
+            result[key] = obj[key]
+        }else{
+            if(Array.isArray(obj[key])){
+                obj[key].forEach((element,i) => {
+                    if(typeof element !== "object"){
+                        tempArray[i] = element
+                    }
+                    else{
+                        let tempObj = deepCloneObj(element)
+                        if(Array.isArray(element)){
+                            tempArray[i] = Object.values(tempObj)
+                        }
+                        else{
+                            tempArray[i] = tempObj
+                        }
+                    }
+                })
+                console.log(tempArray);
+                result[key] = Object.values(tempArray)
+            }
+            else{
+                result[key] = deepCloneObj(obj[key])
+            }
+        }
+    })
+    return result
 }
 
-let merge = function(leftArray, rightArray){
-    let sortedArr = []
-    
-    while(leftArray.length || rightArray.length){
-        if(leftArray.length && rightArray.length){
-            if(leftArray[0] < rightArray[0]){
-                sortedArr.push(leftArray.shift())
-            }else{
-                sortedArr.push(rightArray.shift())
-            }
-        }else if(rightArray.length){
-            sortedArr.push(rightArray.shift())
-        }else if(leftArray.length){
-            sortedArr.push(leftArray.shift())
+const user ={
+    id: 123,
+    name: "Ameer",
+    phone: [123, {a:1, c:{t: [1,2,3] }, b:2} , [1,{c:5},2,3] , 456, 789],
+    address: {
+        country: "Israel", 
+        city: {
+            street:1,
+            house:3
         }
     }
-    return sortedArr;
 }
 
-let arr = [11,1,2,10,5,4,6,7,8,2,9,3,4,1,1,1,1,1];
+let tempObj = deepCloneObj(user)
 
-console.log(mergeSort(arr))
+// user.phone[0] = 5
+// user.address.country = "LA"
+// user.address.city.street = 8
+// user.phone[1].a = 3
+// user.phone[2][0] = 4
+// user.phone[1].c[2] = 45
+user.phone[1].c.t[2] = 45
+
+user.phone[2][1].c = 10
+
+
+user.id = 123456
+
+console.log(user)
+
+console.log(tempObj)
+console.log("aa");
